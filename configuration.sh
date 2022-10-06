1713a6d44e495fc1b65fabe639c9f81c840fc868
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-10-06 12:40:25 -0700

    cleanup

--- configuration.sh
+++ configuration.sh
@@ -3,3 +3,22 @@ MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE
 MY_DIR="$(dirname $MY_PATH)"
 
 BINARY_DIR=${MY_DIR}/binary
+
+## PLATFORM TARGETS
+NODE_VERSION=node14
+TARGETS=()
+
+TARGETS+=("${NODE_VERSION}-alpine-x64")
+TARGETS+=("${NODE_VERSION}-alpine-arm64")
+
+TARGETS+=("${NODE_VERSION}-linux-x64")
+TARGETS+=("${NODE_VERSION}-linux-arm64")
+
+TARGETS+=("${NODE_VERSION}-linuxstatic-x64")
+TARGETS+=("${NODE_VERSION}-linuxstatic-arm64")
+
+TARGETS+=("${NODE_VERSION}-win-x64")
+TARGETS+=("${NODE_VERSION}-win-arm64")
+
+TARGETS+=("${NODE_VERSION}-macos-x64")
+TARGETS+=("${NODE_VERSION}-macos-arm64")
--- package-only.sh
+++ package-only.sh
@@ -7,30 +7,11 @@ source configuration.sh
 
 rm -rf ${BINARY_DIR}/*
 
-NODE_VERSION=node14
-
-TARGETS=()
-
-TARGETS+=("${NODE_VERSION}-alpine-x64")
-TARGETS+=("${NODE_VERSION}-alpine-arm64")
-
-TARGETS+=("${NODE_VERSION}-linux-x64")
-TARGETS+=("${NODE_VERSION}-linux-arm64")
-
-TARGETS+=("${NODE_VERSION}-linuxstatic-x64")
-TARGETS+=("${NODE_VERSION}-linuxstatic-arm64")
-
-TARGETS+=("${NODE_VERSION}-win-x64")
-TARGETS+=("${NODE_VERSION}-win-arm64")
-
-TARGETS+=("${NODE_VERSION}-macos-x64")
-TARGETS+=("${NODE_VERSION}-macos-arm64")
-
 echo "TARGETS: ${TARGETS[@]}"
 
 TARGETS_A=${TARGETS[*]}
 TARGETS_STR=${TARGETS_A// /,}
-# echo "TARGETS_STR: ${TARGETS_STR}"
+echo "TARGETS_STR: ${TARGETS_STR}"
 
 pkg \
     --output ${BINARY_DIR}/kubevious \
