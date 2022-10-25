ea199b7a2ab3fd60f9d6122c2fa37aadfd9c6939
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-10-25 16:01:32 -0700

    cleanup of dev commands

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { logger } from './logger';
 import VERSION from './version'
 
 import { program } from 'commander';
-import setupExtractManifests from './commands/extract-manifests';
 import setupLintManifests from './commands/lint-manifests';
 import setupListKnownK8sVersions from './commands/list-known-k8s-versions';
 
@@ -16,7 +15,6 @@ program
   .description('Kubevious CLI validates Kubernetes manifests.\nFind more information at: https://github.com/kubevious/cli')
   .version(VERSION);
 
-setupExtractManifests(program);
 setupLintManifests(program);
 setupListKnownK8sVersions(program);
 
--- version.sh
+++ version.sh
@@ -1,2 +1,2 @@
 # This file is generated and updated from CI/CD
-export PRODUCT_VERSION=0.0.12
+export PRODUCT_VERSION=1.0.0
