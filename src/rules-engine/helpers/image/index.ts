84c8d4f631aec432025df14686bc8870907cd3f3
Ruben Hakopian <cicd@kubevious.io>
2022-12-02 11:47:21 -0800

    enhanced image parsing.

--- src/rules-engine/helpers/image/index.ts
+++ src/rules-engine/helpers/image/index.ts
@@ -1,21 +1,51 @@
 export function parseImage(fullImage: string) : ImageTag
 {
-    const tagIndex = fullImage.indexOf(':');
-    if (tagIndex === -1) {
+    const match = fullImage.match(/^(?:([^/]+)\/)?(?:([^/]+)\/)?([^@:/]+)(?:[@:](.+))?$/);
+    if (!match) {
         return {
+            isInvalid: true,
+            registry: '',
+            namespace: '',
+            name: fullImage,
+            repository: fullImage,
             image: fullImage,
-            tag: 'latest'
-        }
-    } else {
-        return {
-            image: fullImage.substring(0, tagIndex),
-            tag: fullImage.substring(tagIndex + 1)
+            tag: 'latest',
         }
     }
+
+    let registry : string | undefined = match[1];
+    let namespace = match[2]
+    const name = match[3]
+    const tag = match[4] || 'latest';
+
+    if (!namespace && registry && !/[:.]/.test(registry)) {
+        namespace = registry
+        registry = undefined
+    }
+
+    registry = registry || 'docker.io'
+
+    const repository = [namespace, name].filter(x => x).join('/');
+
+    const result : ImageTag = {
+        registry: registry,
+        namespace: namespace,
+        name: name,
+        repository: repository,
+        image: repository,
+        tag: tag
+    }
+
+    return result
 }
 
 export interface ImageTag
 {
+    isInvalid?: boolean,
+    registry: string,
+    namespace?: string | undefined,
+    name: string,
+    repository: string,
     image: string,
     tag: string,
 }
