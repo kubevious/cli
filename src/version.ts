9876a7294f8f87eaab9b174303c32d4b2954ccce
Kubevious CI/CD <cicd@kubevious.io>
2023-05-04 03:10:10 +0000

    Version Release v1.0.55

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
 // This file is generated and updated from CI/CD
-export default "v1.0.54";
+export default "v1.0.55";
--- version.sh
+++ version.sh
@@ -1,2 +1,2 @@
 # This file is generated and updated from CI/CD
-export PRODUCT_VERSION=1.0.54
+export PRODUCT_VERSION=1.0.55
